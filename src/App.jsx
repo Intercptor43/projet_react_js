422127bc03a8a575cd0cff757e986e8b3621b7eb
Enla <enla@enlapxy.com>
2025-04-16 13:06:08 +0200

    changes to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,13 @@ function App() {
           <ThemeToggle />
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <TalkForm />
-        </div>
-        <div>
-          <TalkList />
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-8">
+          <div>
+            <TalkForm />
+          </div>
+          <div className>
+            <TalkList />
+          </div>
         </div>
       </div>
     </div>
